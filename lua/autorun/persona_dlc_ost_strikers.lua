a647ab91c4e89726e110b7d406988ea8a4b57625
Cpt-Hazama <Caleb123459@gmail.com>
2022-06-20 02:04:38 -0400

    a

---
+++ lua/autorun/persona_dlc_ost_strikers.lua
@@ -0,0 +1,13 @@
+/*--------------------------------------------------
+	*** Copyright (c) 2012-2021 by Cpt. Hazama, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+--------------------------------------------------*/
+if (!file.Exists("autorun/persona_main.lua","LUA")) then return end
+
+P_AddBattleTrack("Axe To Grind (Strikers)","cpthazama/persona5_strikers/music/axe_to_grind.mp3",353)
+P_AddBattleTrack("Dare Devil (Strikers)","cpthazama/persona5_strikers/music/dare_devil.mp3",255)
+P_AddBattleTrack("Last Surprise (Strikers)","cpthazama/persona5_strikers/music/last_surprise.mp3",478)
+P_AddBattleTrack("What You Wish For (Strikers)","cpthazama/persona5_strikers/music/what_you_wish_for.mp3",181)
+
+P_AddBattleTrack("Blooming Villain (Strikers)","cpthazama/persona5_strikers/music/blooming_villain.mp3",220,true)
